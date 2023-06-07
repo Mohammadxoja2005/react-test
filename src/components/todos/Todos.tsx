a83e31f739af20f47060b77617e4cb3e0576b089
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-08 02:14:13 +0500

    added data fetching from json placeholder and added loader to task component

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,16 +1,29 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+// styles
 import styles from "./index.module.scss";
+// mobx
+import { observer } from 'mobx-react-lite';
+import todos from '../../store/todos';
 
-const Todos: FC = () => {
+const Todos: FC = observer(() => {
+
+    useEffect(() => {
+        todos.fetchTodosFn()
+    }, [])
+
+
     return (
         <div className={styles.todos}>
             <div className={styles.todos_container}>
-                <div className={styles.todos_list}>
-                    <p className={styles.todos_list_name} >name</p>
-                </div>
+                {!todos.isLoading
+                    ?
+                    <div className={styles.todos_list}>
+                        <p className={styles.todos_list_name}>{'name'}</p>
+                    </div>
+                    : <div>Loading...</div>}
             </div>
         </div>
     )
-}
+})
 
 export default Todos
---
+++ src/store/todos.ts
@@ -0,0 +1,27 @@
+import { makeAutoObservable } from "mobx";
+// axios
+import axios from 'axios';
+
+class Todos {
+    fetchedTodos: any = [];
+    isLoading: boolean = false;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    fetchTodosFn = async () => {
+        this.isLoading = true;
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=10`);
+            this.fetchedTodos = response.data;
+        } catch (error) {
+            console.error("Failed to fetch todos:", error);
+        } finally {
+            this.isLoading = false;
+        }
+    };
+
+}
+
+export default new Todos()
